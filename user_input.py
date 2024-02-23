093731074258cf02c2144188d5ce4208d54540bb
Fadahunsi Adewunmi <fadaironborn@gmail.com>
2024-02-23 08:16:36 +0000

    assignment 1

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# week one assignment
+name = input ('what is your name: ')
+age = input ('please supply your age: ')
+location = input('where do you live: ')
+
+print ('hello'  +  name  +  'you are'  +  age  +  'years old'  +  'and you live in'  +  location)
